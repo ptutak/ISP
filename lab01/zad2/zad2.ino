e4fe7363ef829f07f01d6726ca31024b313d6f7e
Piotr Tutak <p.tutak@outlook.com>
2020-04-03 21:39:24 +0200

    initial

---
+++ lab01/zad2/zad2.ino
@@ -0,0 +1,47 @@
+
+const double VOLTAGE_BOUNDARY = 3.3;
+
+int sensorPin = A0;
+int ledPin = A1;
+int buttonPin = A2;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  digitalWrite(ledPin, LOW);
+}
+
+int checkButton(int buttonPin){
+  static int lastButtonState = LOW; 
+  static unsigned long timeRead = 0;
+
+  int buttonReading = digitalRead(buttonPin);
+
+  if (buttonReading != lastButtonState) {
+    timeRead = millis();
+  }
+
+  if ((millis() - timeRead) > 50) {
+    lastButtonState = buttonReading;
+  }
+  return lastButtonState;
+}
+
+void loop() {
+  int sensorValue = analogRead(sensorPin);
+  
+  if (sensorValue > VOLTAGE_BOUNDARY) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+
+  int buttonState = checkButton(buttonPin);
+
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, LOW);
+  }
+
+  delay(1);
+}
